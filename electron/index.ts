0a2966a06330c5d86f3d9ab063a2e0d68a4c8342
yuuumbk <hikoki.y@gmail.com>
2022-10-27 11:50:48 +0900

    change: App window size

--- electron/index.ts
+++ electron/index.ts
@@ -7,21 +7,28 @@ import isDev from 'electron-is-dev';
 
 const height = 1500;
 const width = 2000;
+const minHeight = 1200;
+const minWidth = 1600;
 
 function createWindow() {
   // Create the browser window.
   const window = new BrowserWindow({
     width,
     height,
+    minHeight,
+    minWidth,
     //  change to false to use AppBar
     frame: true,
     show: true,
     resizable: true,
     fullscreenable: true,
+    fullscreen: true,
     webPreferences: {
       preload: join(__dirname, 'preload.js')
     }
   });
+
+  window.maximize();
 
   const port = process.env.PORT || 3000;
   const url = isDev ? `http://localhost:${port}` : join(__dirname, '../src/out/index.html');
