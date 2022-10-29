cc2a03610c3d0220fa2d4df5cbc95e89d1be276c
yuuumbk <hikoki.y@gmail.com>
2022-10-30 04:09:28 +0900

    add: bingo voices

--- src/sounds/bingo.ts
+++ src/sounds/bingo.ts
@@ -1,7 +1,33 @@
+import 始めの番号は from './src/bingo/始めの番号は.wav';
 import 次の番号は from './src/bingo/次の番号は.wav';
+import はいはいー次はーー from './src/bingo/はいはいー次はーー.wav';
+import さてさて次の番号はーー from './src/bingo/さてさて次の番号はーー.wav';
+import さてさてお次はー from './src/bingo/さてさてお次はー.wav';
+import 次はどれにしようかなんー from './src/bingo/次はどれにしようかなんー.wav';
+import 次は何かなえーと from './src/bingo/次は何かなえーと.wav';
+import 次に選ぶのは from './src/bingo/次に選ぶのは.wav';
+import 選ばれたのは from './src/bingo/選ばれたのは.wav';
+import 次はこの番号で決まり from './src/bingo/次はこの番号で決まり.wav';
+import どれにしようかなえーと from './src/bingo/どれにしようかなえーと.wav';
+import そうだな次の番号は from './src/bingo/そうだな次の番号は.wav';
+import 次はこれだね from './src/bingo/次はこれだね.wav';
+import 次はどれがいいかな from './src/bingo/次はどれがいいかな.wav';
 import 番です from './src/bingo/番です.wav';
 
 export const bingo = {
+  始めの番号は,
   次の番号は,
+  はいはいー次はーー,
+  さてさて次の番号はーー,
+  さてさてお次はー,
+  次はどれにしようかなんー,
+  次は何かなえーと,
+  次に選ぶのは,
+  選ばれたのは,
+  次はこの番号で決まり,
+  どれにしようかなえーと,
+  そうだな次の番号は,
+  次はこれだね,
+  次はどれがいいかな,
   番です
 };
