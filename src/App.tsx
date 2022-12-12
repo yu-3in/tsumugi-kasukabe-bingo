5f6438de0e8d05aba18449d31d17d66b24a478f9
yuuumbk <hikoki.y@gmail.com>
2022-12-12 11:24:57 +0900

    delete: Rules page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import Rules from './pages/Rules';
 import Home from './pages/Home';
 import Bingo from './pages/Bingo';
 import { AvatarProvider } from './contexts/AvatarContext';
 import SettingsBingo from './pages/Settings/SettingsBingo';
 import SettingsGeneral from './pages/Settings/SettingsGeneral';
-import SettingsRule from './pages/Settings/SettingsRule';
 import SettingsChar from './pages/Settings/SettingsChar';
 import SettingsVoice from './pages/Settings/SettingsVoice';
 
@@ -16,12 +14,10 @@ const App: React.FC = () => {
       <HashRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/rules" element={<Rules />} />
           <Route path="/bingo" element={<Bingo />} />
           <Route path="/settings">
             <Route path="general" element={<SettingsGeneral />} />
             <Route path="bingo" element={<SettingsBingo />} />
-            <Route path="rule" element={<SettingsRule />} />
             <Route path="char" element={<SettingsChar />} />
             <Route path="voice" element={<SettingsVoice />} />
           </Route>
