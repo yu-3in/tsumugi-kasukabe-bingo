beb955e65cf34b776450db36e921810dc4badd2e
yuuumbk <hikoki.y@gmail.com>
2022-12-12 11:09:06 +0900

    add: Create a ball color inversion mode

--- src/components/Bingo/Ball.tsx
+++ src/components/Bingo/Ball.tsx
@@ -4,6 +4,7 @@ import { Colors } from '../../types/Colors';
 import { cn } from '../../utils/cn';
 import { bgColor } from '../../utils/css/bgColor';
 import { borderColor } from '../../utils/css/borderColor';
+import { textColor } from '../../utils/css/textColor';
 
 type Props = {
   type: 'notHit' | 'hit' | 'history';
@@ -20,7 +21,8 @@ const Ball: React.FC<Props> = ({ type, num, color, borderWidth = 8, className })
         className,
         'rounded-full  w-[calc(100vh/15.5)] h-[calc(100vh/15.5)] text-4xl flex items-center justify-center hover:cursor-pointer',
         type === 'notHit' && `${borderColor(color)} bg-[#fff] text-gray-400 opacity-80`,
-        type === 'hit' && `border-none ${bgColor(color)} text-gray-700`,
+        // type === 'hit' && `${borderColor(color)} text-gray-700 bg-white`,
+        type === 'hit' && `border-none ${textColor(color)} bg-gray-700`,
         type == 'history' && `${borderColor(color)} bg-[#fff] text-gray-400`
       )}
       style={{ borderWidth }}
